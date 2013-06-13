e4bf56c3019886287459ad312603a1482eec0876
Joseph <joewestwood159@gmail.com>
2013-06-13 16:59:27 +0100

    Updates

--- JS/validator_rules.js
+++ JS/validator_rules.js
@@ -1,24 +1,15 @@
-// 
-// Name:	ukPhone
-// Author: 	jwestwood
-// Desc: 	Validates a UK landline or mobile telephone number
-//
 $.validator.addMethod(
-	"ukPhone", 
+	"ukPhone",
 	function(value, element){
 		if( value.length )
 			return	/^(\+44|0|00|044|0044)(1|2|7)(\d\s?){9}$/.test(value.replace(/\s+/g, ''));
 		else
 			return true;
-	}, 
+	},
 	"Please enter a valid UK phone number"
 );
 
-//
-// Name: 	minAge
-// Author: 	jwestwood
-// Desc: 	Checks that a date in the format YYYY-MM-DD is at least X years old
-//
+// requires ISO date YYYY-MM-DD
 $.validator.addMethod(
 	"minAge",
 	function(value, el, min){
@@ -26,7 +17,6 @@ $.validator.addMethod(
 		{
 			var now = new Date(), bday = new Date(value);
 			var age = ( now - bday ) / 31557600000;
-			console.log(now, bday, age);
 			return age >= parseInt(min);
 		}
 		else
@@ -35,11 +25,6 @@ $.validator.addMethod(
 	"You are not old enough"
 );
 
-// 
-// Name: 	website
-// Author: 	jwestwood
-// Desc: 	Validates a website url allowing filepaths, subdomains, querystring and optional http/https protocol
-//
 $.validator.addMethod(
 	"website",
 	function(value, el, param){
@@ -51,14 +36,10 @@ $.validator.addMethod(
 	"Please enter a valid web address"
 );
 
-//
-// Name: 	ukPostcode
-// Author: 	jwestwood
-// Desc: 	Validates a UK postcode according to the format standard, spaces and lowercase permitted
-//
 $.validator.addMethod(
 	"ukPostcode",
-	function(value, el, param){
+	function(value, el, allow_half){
+
 		// uppercase
 		value = value.toUpperCase();
 		switch( value.replace(/\s+/g, '').length )
@@ -66,11 +47,20 @@ $.validator.addMethod(
 			// don't fail on empty postcodes (use required:true if they are required)
 			case 0: return true;
 
+			// A9
+			case 2: return allow_half && /^[A-Z]\d ?$/.test(value);
+
+			// A9A
+			case 3: return allow_half && /^[A-Z][A-Z\d][A-Z\d] ?$/.test(value);
+
+			// AA9A
+			case 4: return allow_half && /^[A-Z][A-Z]\d[A-Z\d] ?$/.test(value);
+
 			// A9 9AA
 			case 5: return /^[A-Z]\d ?\d[A-Z][A-Z]$/.test(value);
 
 			// A9A 9AA  or  A99 9AA  or  AA9 9AA
-			case 6: return /^[A-Z][A-Z\d][A-Z\d] \d[A-Z][A-Z]$/.test(value);
+			case 6: return /^[A-Z][A-Z\d][A-Z\d] ?\d[A-Z][A-Z]$/.test(value);
 
 			// AA9A 9AA  or  AA99 9AA
 			case 7: return /^[A-Z][A-Z]\d[A-Z\d] ?\d[A-Z][A-Z]$/.test(value);
